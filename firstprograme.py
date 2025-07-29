2fcc3c62c16b0ff034497d6f4702460d87ffa8da
rinchenzangpo <zangporinchen2006@gmail.com>
2025-07-29 14:08:30 +0530

    my identity

--- firstprograme.py
+++ firstprograme.py
@@ -1,4 +1,7 @@
 print("Hello SWE2025")
 a=2
 b=3
-print(a+b)
+print("the sum of and b : ",a+b)
+name=input("enter your name:")
+age=input("enter ur age:")
+print("u r ",name,"and u r",age,"years old")
