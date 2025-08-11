782aa63082ef609c46627297efce90fb721e6c6a
rinchenzangpo <zangporinchen2006@gmail.com>
2025-08-11 12:57:36 +0530

    my second edit with string

--- csf101-python_exercises/basics/strings.py
+++ csf101-python_exercises/basics/strings.py
@@ -4,3 +4,10 @@ greeting_f="hello, " + name + "!"
 print(greeting_f)
 name_length=len(name)
 print(name_length)
+non_num_str = "hello"
+try:
+    non_num_str = int(non_num_str)
+    print(non_num_str)
+except ValueError as e:
+    print(f"Error: {e}")
+    
